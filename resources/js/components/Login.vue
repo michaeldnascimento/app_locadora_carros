90ade2d345fc555a8c34c1425a110be03ef7d40f
Michael D Nascimento <you@example.com>
2022-01-09 21:00:56 -0300

    Vue props e npm run watch

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -3,11 +3,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Login (component Vue)"</div>
+                    <div class="card-header">Login (> run watch - Vue)"</div>
 
                     <div class="card-body">
                         <form method="POST" action="">
 
+                            <input type="hidden" name="_token" :value="csrf_token">
                             <div class="row mb-3">
                                 <label for="email" class="col-md-4 col-form-label text-md-end">E-mail</label>
 
@@ -58,5 +59,12 @@
 </template>
 
 <script>
+
+    export default {
+        props: [
+            'csrf_token'
+        ]
+
+    }
 
 </script>
